411f57d1c78671982eaf990ba22513b30fb6ba71
bingtaoo <1007530194@qq.com>
2023-04-16 21:51:58 +0800

    update

--- notefluid/common/base/cache.py
+++ notefluid/common/base/cache.py
@@ -11,13 +11,13 @@ class BaseCache:
     def exists(self):
         return os.path.exists(self.filepath)
 
-    def execute(self):
+    def execute(self, *args, **kwargs):
         raise Exception("not implement.")
 
-    def read(self):
+    def read(self, *args, **kwargs):
         raise Exception("not implement.")
 
-    def write(self):
+    def write(self, *args, **kwargs):
         raise Exception("not implement.")
 
 
@@ -26,12 +26,12 @@ class CSVCache(BaseCache):
         super(CSVCache, self).__init__(*args, **kwargs)
         self.df = None
 
-    def read(self):
+    def read(self, *args, **kwargs):
         if self.overwrite or not self.exists():
             self.df = self.execute()
         if self.df is None:
             self.df = pd.read_csv(self.filepath)
         return self.df
 
-    def write(self):
+    def write(self, *args, **kwargs):
         self.df.to_csv(self.filepath, index=None)
