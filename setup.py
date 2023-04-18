fb7ef3e40c4e9a3be42a03ff72dc80bd2f5c9806
bingtaoo <1007530194@qq.com>
2023-04-18 21:25:23 +0800

    add

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
-from os import path
+import time
 
-from notebuild.tool import read_version
 from setuptools import find_packages, setup
-
-version_path = path.join(path.abspath(path.dirname(__file__)), 'script/__version__.md')
-
-version = read_version(version_path)
 
 install_requires = ['numpy', 'scipy']
 
 setup(name='notefluid',
-      version=version,
+      version=time.strftime("%Y%m%d%H%M", time.localtime()),
       description='notefluid',
       author='niult',
       author_email='1007530194@qq.com',
