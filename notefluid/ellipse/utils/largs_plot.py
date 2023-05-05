fabe3b0d7dd0d5a27e07ec0ea0484351dd248fb7
bingtaoo <1007530194@qq.com>
2023-05-05 20:26:52 +0800

    update

--- notefluid/ellipse/utils/largs_plot.py
+++ notefluid/ellipse/utils/largs_plot.py
@@ -10,6 +10,7 @@ def largs_plot_speed(df):
                  linewidth=0.5, head_width=0.5,
                  head_length=0.5)
 
+    plt.plot(df['lx'], df['ly'], '-o')
     df.apply(plot_larg, axis=1)
     plt.xlim(df['lx'].min() - df['ux'].abs().max() - 5, df['lx'].max() + df['ux'].abs().max() + 5)
     plt.ylim(df['ly'].min() - df['uy'].abs().max() - 5, df['ly'].max() + df['uy'].abs().max() + 5)
