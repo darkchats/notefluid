851c3a325bc0674c314293a2dec6aad54cf83466
niuliangtao <niuliangtao@qq.com>
2023-08-25 14:33:00 +0800

    add

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-import time
+from funpypi import setup
 
-from setuptools import find_packages, setup
+install_requires = ["numpy", "scipy", "pandas", "matplotlib"]
 
-install_requires = ['numpy', 'scipy','pandas','matplotlib']
-
-setup(name='funfluid',
-      version=time.strftime("%Y%m%d%H%M", time.localtime()),
-      description='funfluid',
-      author='bingtao',
-      author_email='1007530194@qq.com',
-      url='https://github.com/1007530194',
-
-      packages=find_packages(),
-      install_requires=install_requires
-      )
+setup(name="funfluid", install_requires=install_requires)
