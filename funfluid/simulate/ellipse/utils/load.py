940f26e8c9382c4b2e942696c54f01966b863ed2
niuliangtao <niuliangtao@qq.com>
2023-10-29 13:52:18 +0800

    add

--- funfluid/simulate/ellipse/utils/load.py
+++ funfluid/simulate/ellipse/utils/load.py
@@ -6,9 +6,13 @@ def load_v(path):
         row1 = fr.readline()
         row2 = fr.readline()
     parameter_map = dict(kv.strip().split('=') for kv in row2.strip("zone").split(','))
-    df = pd.read_csv(path, skiprows=1, nrows=int(parameter_map['I']) * int(parameter_map['J']) - 1, sep='\s+')
+
+    df = pd.read_csv(path, skiprows=2, header=None,
+                     nrows=int(parameter_map['I']) * int(parameter_map['J']), sep='\s+')
+
     cols = row1.strip('ZIBE').split('=')[1].strip().replace('"', '').split(',')
     cols = [col for col in cols if len(col) > 0]
+
     df.columns = cols
     df[['x', 'y']] = df[['x', 'y']].astype('int')
     return df
@@ -19,6 +23,6 @@ def load_p(path):
         row1 = fr.readline()
         row2 = fr.readline()
     parameter_map = dict(kv.strip().split('=') for kv in row2.split(','))
-    df = pd.read_csv(path, skiprows=1, nrows=int(parameter_map['E']), sep='\s+')
+    df = pd.read_csv(path, skiprows=2, header=None, nrows=int(parameter_map['E']) + 1, sep='\s+')
     df.columns = row1.split('=')[1].strip().replace('"', '').split(',')
     return df
